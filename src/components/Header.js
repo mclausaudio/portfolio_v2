7a8f7a53481dfdc87fb1c44acdf9b94d0f785f3d
Michael_Claus <mclausaudio@gmail.com>
2019-08-18 10:47:57 -0700

    initial commit

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,19 +4,23 @@ import Footer from './Footer'
 import avatar from '../assets/images/avatar.jpg'
 
 class Header extends React.Component {
-    render() {
-        return (
-            <header id="header">
-                <div className="inner">
-                    <a href="#" className="image avatar"><img src={avatar} alt="" /></a>
-                    <h1><strong>I am Strata</strong>, a super simple<br />
-                    responsive site template freebie<br />
-                    crafted by <a href="http://html5up.net">HTML5 UP</a>.</h1>
-                </div>
-                <Footer />
-            </header>
-        )
-    }
+  render() {
+    return (
+      <header id="header">
+        <div className="inner">
+          <a href="#" className="image avatar">
+            <img src={avatar} alt="" />
+          </a>
+          <h1>
+            <strong>I am Michael</strong>, a software developer <br />
+            and sound designer from San Francisco, CA.
+            <br />
+          </h1>
+        </div>
+        <Footer />
+      </header>
+    )
+  }
 }
 
 export default Header
